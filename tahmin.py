4d58223e388efe50f535317f3f5e886d3e2be8ce
Aydoğan <aydogan.gokberk@gmail.com>
2020-05-26 20:27:20 +0300

    Delete tahmin.py

--- tahmin.py
+++
@@ -1,9 +0,0 @@
-gizliKelime = "fox"
-tahmin = ""
-tahminSayisi = 0
-maxTahmin = 5
-while tahmin != gizliKelime:
-    tahmin = input("Bilemedin! Gizli kelime ne? ")
-    tahminSayisi += 1
-
-print("Gizli kelimeyi buldun!")
